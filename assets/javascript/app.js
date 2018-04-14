9cd2c62d37ed117b391e6d15eb7c2393a2cb908a
Mikala Smith <mikalasmith@wifi_guest-10-38-127-40.uncc.edu>
2018-04-14 17:28:08 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
 //define variables
-var correct = correct;
-var incorrect = incorrect;
-var unanswered = unanswered;
-var timer = timer;
+var correct;
+var incorrect;
+var unanswered;
+var timer;
 
 //define functions
-//check to see if each question has been answered or unanswered
+//check to see if each question has been answered or unanswerd  
 function checkStatus(){
-  if ($( "input" ).on( "click", function(){
+  if ($( "input" ).on( "click", function()){
     $( "type=radio" ).html( $( "input:checked" ).val() + " is checked!" );
 }
