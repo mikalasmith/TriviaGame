be4ed575ae9349ff181b8f15278c5bd0122b9a40
Mikala Smith <mikalasmith@mikalas-air.home>
2018-04-27 18:21:40 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,11 +37,11 @@ var questions = [
 var game = {
   correct: 0,
   incorrect: 0, 
-  counter: 0, 
+  counter: 120, 
   countdown: function(){
     game.counter--;
     $('#counter').html(game.counter);
-    if(game.counter<=0){
+    if(game.counter == 0){
       console.log('Time is up!');
       game.done();
     }
@@ -49,49 +49,52 @@ var game = {
   
   start: function (){
     timer = setInterval(game.countdown,1000);
-    $("#subwrapper").prepend('<h2> Time Remaining: <span id="counter">120</span> Seconds</h2>');
-    $("#start").on("click",function(){
+    $("#subwrapper").html('<h2> Time Remaining: <span id="counter">120</span> Seconds</h2>');
+   //$("#start").on("click",function(){
       $("#start").remove();
       for (var i=0; i < questions.length; i++){
         $("#subwrapper").append("<h2>" + questions[i].question + "</h2>")
         for (var j=0; j < questions[i].answers.length; j++){
-          $("#subwrapper").append("<input type='radio' name='question-"+ i + "' value= '" + questions[i].answers[j]+"'>"+questions[i].answers[j])
+          $("#subwrapper").append("  <input type='radio' name='question-"+ i + "' value= '" + questions[i].answers[j]+"'>"+questions[i].answers[j] )
         }
       }
       $('#subwrapper').append('<br> <button id="end">DONE</button>');
-    })
+    //})
 
   },
   done: function(){
-    $.each($('input[name="question-0]":checked'),function(){
+    console.log("DONE");
+    $.each($('input[name="question-0"]:checked'),function(){
       if ($(this).val()==questions[0].correctAnswer){
+      
         game.correct++;
+        console.log(game.correct)
       } else {
         game.incorrect++;
       }
     });
-    $.each($('input[name="question-1]":checked'),function(){
+    $.each($('input[name="question-1"]:checked'),function(){
       if ($(this).val()==questions[1].correctAnswer){
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($('input[name="question-2]":checked'),function(){
+    $.each($('input[name="question-2"]:checked'),function(){
       if ($(this).val()==questions[2].correctAnswer){
         game.correct++;
       } else {
-        game.incorrect++;
-      }
+        game.incorrect++
+      } 
     });
-    $.each($('input[name="question-3]":checked'),function(){
+    $.each($('input[name="question-3"]:checked'),function(){
       if ($(this).val()==questions[3].correctAnswer){
         game.correct++;
       } else {
         game.incorrect++;
       }
     });
-    $.each($('input[name="question-4]":checked'),function(){
+    $.each($('input[name="question-4"]:checked'),function(){
       if ($(this).val()==questions[4].correctAnswer){
         game.correct++;
       } else {
@@ -101,8 +104,9 @@ var game = {
     this.result();
   },
   result: function(){
+    console.log("RESULTS");
     clearInterval(timer);
-    $('#subwrapper <h2>').remove();
+    $('<h2> #subwrapper </h2>').remove();
     $('#subwrapper').html("<h2>All done!</h2>");
     $('#subwrapper').append("<h3> Correct Answers: " + this.correct+"</h3>");
     $('#subwrapper').append("<h3> Incorrect Answers: " + this.incorrect+"</h3>");
